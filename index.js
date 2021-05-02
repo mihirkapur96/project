64a2427d3a5a3e63a9b12020c8defea68397282e
Mihir Kapur <mihirkapur96@gmail.com>
2021-05-02 13:55:48 -0700

    setup

---
+++ index.js
@@ -0,0 +1,37 @@
+// Required modules 
+const express = require("express");
+const app = express();
+const dblib = require("./dblib.js");
+
+const multer = require("multer");
+const upload = multer();
+
+// Add middleware to parse default urlencoded form
+app.use(express.urlencoded({ extended: false }));
+
+// Setup EJS
+app.set("view engine", "ejs");
+
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+// Application folders
+app.use(express.static("public"));
+
+// Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started (http://localhost:3000/) !");
+});
+
+// Setup routes
+app.get("/", (req, res) => {
+    //res.send("Root resource - Up and running!")
+    res.render("index");
+});
